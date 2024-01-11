eeb92bb9b644924eeebd41d86c526c6ae374b389
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2024-01-11 17:23:24 +0800

    Retrieve expected data such as width and elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './assets/component-one/style.css';
 import dropDown from '@aloe_vera/drop-down-func/package/nav-func';
+import { navigationLinks } from './js/progressive-collapsing-menu';
 
 window.addEventListener('load', () => {
   const moreLinks = document.querySelectorAll('a[data-more-menu-link]');
@@ -10,6 +11,17 @@ window.addEventListener('load', () => {
     subElement: 'div[data-more-link-inside',
     classListValue: 'visible',
     togglerClassListValue: 'active-toggler',
-    closerToggler: '#main_container',
+    closerToggler: '.close_toggler > div',
   }).dropDownElement();
+});
+
+const allNavListOfLinks = document.querySelectorAll('li[class="contain_link"]');
+const allNavAnchorLinks = document.querySelectorAll('a[data-menu-link]');
+
+const navLinks = navigationLinks();
+navLinks.addNavigationListOfLinks(allNavListOfLinks);
+navLinks.itsLinkWidthGetter(allNavAnchorLinks);
+
+window.addEventListener('resize', () => {
+  console.log(navLinks.navigationListOfLinksArray, navLinks.anchorLinkWidth);
 });
