867b107c18b8666557120cb44d6ab01c2a886f18
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2024-01-12 16:37:48 +0800

    Todo: fix individual variable scoping

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './assets/component-one/style.css';
 import dropDown from '@aloe_vera/drop-down-func/package/nav-func';
-import { navigationLinks } from './js/progressive-collapsing-menu';
+import {
+  navigationLinks,
+  elementMovementOnCondition,
+  moveElementFromOtherArrayToArray,
+  populateElement,
+} from './js/progressive-collapsing-menu';
 
 window.addEventListener('load', () => {
   const moreLinks = document.querySelectorAll('a[data-more-menu-link]');
@@ -15,15 +20,34 @@ window.addEventListener('load', () => {
   }).dropDownElement();
 });
 
+const unorderedLinks = document.querySelector('ul[class="unordered_links"]');
 const allNavListOfLinks = document.querySelectorAll('li[class="contain_link"]');
-const allNavAnchorLinks = document.querySelectorAll('a[data-menu-link]');
+const moreUnorderedLinks = document.querySelector(
+  'ul[class="more_unordered_links"]',
+);
 
 const navLinks = navigationLinks();
+const anchorLinkWidths = navLinks.itsLinkWidthGetter();
 navLinks.addNavigationListOfLinks(allNavListOfLinks);
 
 window.addEventListener('resize', () => {
-  console.log(
-    navLinks.navigationListOfLinksArray,
-    navLinks.itsLinkWidthGetter(allNavAnchorLinks),
-  );
+  elementMovementOnCondition({
+    moveFunctions: moveElementFromOtherArrayToArray({
+      navigationLinksArray: navLinks.navigationListOfLinksArray,
+      moreNavigationLinksArray: navLinks.moreNavigationListOfLinksArray,
+    }),
+    navigationLinksWidth: unorderedLinks.clientWidth,
+    navigationParentWidth: anchorLinkWidths.clientWidth,
+    additionalValue: 100,
+  }).move();
+
+  populateElement({
+    arrayOfElement: navLinks.navigationListOfLinksArray,
+    parent: unorderedLinks,
+  }).populate();
+
+  populateElement({
+    arrayOfElement: navLinks.moreNavigationListOfLinksArray,
+    parent: moreUnorderedLinks,
+  }).populate();
 });
